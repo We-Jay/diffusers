e73517cf34dc8d5ed00f856a21bc878c2d9f02fb
We-Jay <34061747+We-Jay@users.noreply.github.com>
2023-05-28 17:12:17 +0530

    Update pipeline_stable_diffusion_panorama.py

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
@@ -668,16 +668,16 @@ class StableDiffusionPanoramaPipeline(DiffusionPipeline, TextualInversionLoaderM
                     # save views scheduler status after sample
                     views_scheduler_status[j] = copy.deepcopy(self.scheduler.__dict__)
 
-#                     value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
-#                     value[:, :, h_start:h_end, w_start:w_end] -= value[:, :, h_start:h_end, w_start:w_end]
                     value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
+#                     value[:, :, h_start:h_end, w_start:w_end] -= value[:, :, h_start:h_end, w_start:w_end]
+#                     value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
                     count[:, :, h_start:h_end, w_start:w_end] += 1
 
                 for j, (h_start, h_end, w_start, w_end) in enumerate(views_segment):
                     print(f'iteration = {i} segment no = {j}, and count is = {count[0][0][h_start+1][w_start+1]}')
 
                 # take the MultiDiffusion step. Eq. 5 in MultiDiffusion paper: https://arxiv.org/abs/2302.08113
-#                 latents = torch.where(count > 0, value / count, value)
+                latents = torch.where(count > 0, value / count, value)
 
 
 

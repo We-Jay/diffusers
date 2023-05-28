2c647d4ebc07e92e018edce1ca1a310134974142
We-Jay <34061747+We-Jay@users.noreply.github.com>
2023-05-28 17:35:28 +0530

    Update pipeline_stable_diffusion_panorama.py

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
@@ -449,7 +449,7 @@ class StableDiffusionPanoramaPipeline(DiffusionPipeline, TextualInversionLoaderM
         latents = latents * self.scheduler.init_noise_sigma
         return latents
 
-    def get_views(self, panorama_height, panorama_width, window_size=64, stride=64):
+    def get_views(self, panorama_height, panorama_width, window_size=64, stride=32):
         # Here, we define the mappings F_i (see Eq. 7 in the MultiDiffusion paper https://arxiv.org/abs/2302.08113)
         panorama_height /= 8
         panorama_width /= 8
@@ -668,16 +668,17 @@ class StableDiffusionPanoramaPipeline(DiffusionPipeline, TextualInversionLoaderM
                     # save views scheduler status after sample
                     views_scheduler_status[j] = copy.deepcopy(self.scheduler.__dict__)
 
-                    value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
-#                     value[:, :, h_start:h_end, w_start:w_end] -= value[:, :, h_start:h_end, w_start:w_end]
 #                     value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
+                    value[:, :, h_start:h_end, w_start:w_end] -= value[:, :, h_start:h_end, w_start:w_end]
+                    value[:, :, h_start:h_end, w_start:w_end] += latents_view_denoised
                     count[:, :, h_start:h_end, w_start:w_end] += 1
 
                 for j, (h_start, h_end, w_start, w_end) in enumerate(views_segment):
                     print(f'iteration = {i} segment no = {j}, and count is = {count[0][0][h_start+1][w_start+1]}')
 
                 # take the MultiDiffusion step. Eq. 5 in MultiDiffusion paper: https://arxiv.org/abs/2302.08113
-                latents = torch.where(count > 0, value / count, value)
+#                 latents = torch.where(count > 0, value / count, value)
+                latents = torch.where(count > 0, value, value)
 
 
 
